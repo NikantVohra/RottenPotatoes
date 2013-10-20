bbe6094907abb1dce56c27a34da4110225f2e19c
saasbook <saasbook@saasbook.(none)>
2013-10-20 05:38:27 -0700

    complete HW2

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,20 +1,41 @@
 class MoviesController < ApplicationController
-
+  @@DefaultRatings = {'G' => '1','PG' => '1','PG-13' => 1,'R' => '1','NC-17' => '1'}		
+  
   def show
     id = params[:id] # retrieve movie ID from URI route
     @movie = Movie.find(id) # look up movie by unique ID
     # will render app/views/movies/show.<extension> by default
   end
-  
+
   def index
 
-	if params[:sort] == 'title'
-		@movies = Movie.find(:all,:order => 'title')
-	elsif params[:sort] == 'release_date'
-		@movies = Movie.find(:all,:order => 'release_date')
-	else
-		@movies = Movie.all
-	end	
+	@all_ratings = Movie.find_ratings
+
+	if session[:sort] != params[:sort] and params[:sort]
+      session[:sort] = params[:sort]
+    end
+
+    if session[:ratings] != params[:ratings] and params[:ratings]
+      session[:ratings] = params[:ratings]
+    end
+
+    if !params[:sort] && !params[:ratings] && (session[:sort] || session[:ratings])
+      redirect_to movies_path(:sort => session[:sort], :ratings => session[:ratings])
+    end
+
+	if session[:ratings]
+	  @selected_ratings = session[:ratings].keys
+    else
+      @selected_ratings = @all_ratings
+    end
+
+	
+	@movies = Movie.find_all_by_rating(@selected_ratings)
+
+	if session[:sort] 
+		@movies = Movie.sort_by(session[:sort],@selected_ratings)
+	end
+	
   end
 
   def new
